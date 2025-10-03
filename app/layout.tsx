2fc3acf0d3ee8ba18638526ebd63394887a793d9
lautarodesouches <lautarodesouches@outlook.com>
2025-10-03 10:23:05 -0300

    [Feat] Firebase register

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
+import { AuthProvider } from '@/context/AuthContext'
 
 const plusJakartaSans = Plus_Jakarta_Sans({
     subsets: ['latin'],
@@ -45,7 +46,7 @@ export default function RootLayout({
             <body
                 className={`${plusJakartaSans.className} ${gliker.className}`}
             >
-                {children}
+                <AuthProvider>{children}</AuthProvider>
             </body>
         </html>
     )
---
+++ lib/firebase.ts
@@ -0,0 +1,14 @@
+import { firebaseConfig } from '@/constants'
+import { initializeApp, getApps, getApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+// Make sure Firebase is not initialized multiple times (important in Next.js)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+
+export default app
