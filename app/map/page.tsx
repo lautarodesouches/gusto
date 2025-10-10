9ab1ee9da6a96559a3189fff26c0daedf10f41bb
lautarodesouches <lautarodesouches@outlook.com>
2025-10-10 10:42:48 -0300

    [Feat] Map oading animation

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -10,7 +10,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { HomeFilter, HomeSocial, MapView } from '@/components'
 import { useState } from 'react'
-import { MapProvider } from '@/context/MapProvider'
+import { MapProvider } from '@/components/Home/MapProvider'
 
 export default function Map() {
     const [isFiltersVisible, setIsFiltersVisible] = useState(false)
@@ -27,94 +27,94 @@ export default function Map() {
     }
 
     return (
-        <MapProvider>
-            <main className={styles.main}>
-                <nav className={styles.nav}>
-                    <fieldset className={styles.nav__fieldset}>
-                        <FontAwesomeIcon
-                            icon={faSearch}
-                            className={styles.nav__icon}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Escribe un lugar"
-                            name="search"
-                            className={styles.nav__input}
-                        />
-                    </fieldset>
-                    <div className={styles.nav__div}>
-                        <FontAwesomeIcon
-                            icon={faBell}
-                            className={styles.nav__icon}
-                        />
-                    </div>
-                    <div className={styles.nav__div}>
-                        <FontAwesomeIcon
-                            icon={faUser}
-                            className={styles.nav__icon}
-                        />
-                    </div>
-                </nav>
+        <main className={styles.main}>
+            <nav className={styles.nav}>
+                <fieldset className={styles.nav__fieldset}>
+                    <FontAwesomeIcon
+                        icon={faSearch}
+                        className={styles.nav__icon}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Escribe un lugar"
+                        name="search"
+                        className={styles.nav__input}
+                    />
+                </fieldset>
+                <div className={styles.nav__div}>
+                    <FontAwesomeIcon
+                        icon={faBell}
+                        className={styles.nav__icon}
+                    />
+                </div>
+                <div className={styles.nav__div}>
+                    <FontAwesomeIcon
+                        icon={faUser}
+                        className={styles.nav__icon}
+                    />
+                </div>
+            </nav>
+            <MapProvider>
                 <MapView />
-                <section className={styles.bottom}>
-                    <div className={styles.bottom__container}>
-                        <button
-                            className={styles.bottom__button}
-                            onClick={handleClickFilters}
-                        >
-                            <FontAwesomeIcon
-                                icon={faFilter}
-                                className={styles.bottom__icon}
-                            />
-                            <span className={styles.bottom__span}>Filtros</span>
-                        </button>
-                        <button
-                            className={styles.bottom__button}
-                            onClick={handleClickSocial}
-                        >
-                            <FontAwesomeIcon
-                                icon={faUsers}
-                                className={styles.bottom__icon}
-                            />
-                            <span className={styles.bottom__span}>Social</span>
-                        </button>
-                    </div>
-                    <HomeFilter
-                        isVisible={isFiltersVisible}
-                        handleClose={() => {
-                            setIsFiltersVisible(!isFiltersVisible)
-                        }}
-                    />
-                    <HomeSocial
-                        isVisible={isSocialVisible}
-                        handleClose={() => {
-                            setIsSocialVisible(!isSocialVisible)
-                        }}
-                        friends={[
-                            {
-                                image: '',
-                                name: 'Lautaro Desoches',
-                                user: 'lauti',
-                            },
-                            {
-                                image: '',
-                                name: 'Agustin Cardeli',
-                                user: 'agus',
-                            },
-                        ]}
-                        groups={[
-                            {
-                                name: 'Trabajo',
-                                numberOfMembers: 7,
-                            },
-                            {
-                                name: 'Amigos',
-                                numberOfMembers: 5,
-                            },
-                        ]}
-                    />
-                </section>
-            </main>
-        </MapProvider>
+            </MapProvider>
+            <section className={styles.bottom}>
+                <div className={styles.bottom__container}>
+                    <button
+                        className={styles.bottom__button}
+                        onClick={handleClickFilters}
+                    >
+                        <FontAwesomeIcon
+                            icon={faFilter}
+                            className={styles.bottom__icon}
+                        />
+                        <span className={styles.bottom__span}>Filtros</span>
+                    </button>
+                    <button
+                        className={styles.bottom__button}
+                        onClick={handleClickSocial}
+                    >
+                        <FontAwesomeIcon
+                            icon={faUsers}
+                            className={styles.bottom__icon}
+                        />
+                        <span className={styles.bottom__span}>Social</span>
+                    </button>
+                </div>
+                <HomeFilter
+                    isVisible={isFiltersVisible}
+                    handleClose={() => {
+                        setIsFiltersVisible(!isFiltersVisible)
+                    }}
+                />
+                <HomeSocial
+                    isVisible={isSocialVisible}
+                    handleClose={() => {
+                        setIsSocialVisible(!isSocialVisible)
+                    }}
+                    friends={[
+                        {
+                            image: '',
+                            name: 'Lautaro Desoches',
+                            user: 'lauti',
+                        },
+                        {
+                            image: '',
+                            name: 'Agustin Cardeli',
+                            user: 'agus',
+                        },
+                    ]}
+                    groups={[
+                        {
+                            name: 'Trabajo',
+                            numberOfMembers: 7,
+                        },
+                        {
+                            name: 'Amigos',
+                            numberOfMembers: 5,
+                        },
+                    ]}
+                />
+            </section>
+        </main>
     )
 }
--- components/Home/MapProvider/index.tsx
+++ components/Home/MapProvider/index.tsx
@@ -4,6 +4,7 @@
 // Import necessary modules and functions from external libraries and our own project
 import { Libraries, useJsApiLoader } from '@react-google-maps/api'
 import { ReactNode } from 'react'
+import styles from './page.module.css'
 
 // Define a list of libraries to load from the Google Maps API
 const libraries = ['places', 'drawing', 'geometry']
@@ -16,9 +17,22 @@ export function MapProvider({ children }: { children: ReactNode }) {
         libraries: libraries as Libraries,
     })
 
-    if (loadError) return <p>Encountered error while loading google maps</p>
+    if (loadError) {
+        return (
+            <div className={styles.errorContainer}>
+                <p>Error al cargar Google Maps 😢</p>
+            </div>
+        )
+    }
 
-    if (!scriptLoaded) return <p>Map Script is loading ...</p>
+    if (!scriptLoaded) {
+        return (
+            <div className={styles.loadingContainer}>
+                <div className={styles.spinner}></div>
+                <p className={styles.loadingText}>Cargando mapa...</p>
+            </div>
+        )
+    }
 
     // Return the children prop wrapped by this MapProvider component
     return children
