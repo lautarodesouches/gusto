1849fd5c13164699f244fd7b7f76427397502abb
lautarodesouches <lautarodesouches@outlook.com>
2025-10-10 12:46:58 -0300

    [Fix] Remove jwt

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import jwt from 'jsonwebtoken'
 import { ROUTES } from './routes'
-
-const JWT_SECRET = process.env.JWT_SECRET || 'super-secret'
 
 export function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl
@@ -18,15 +15,6 @@ export function middleware(req: NextRequest) {
             return NextResponse.redirect(url)
         }
 
-        try {
-            // Validar JWT
-            jwt.verify(token, JWT_SECRET)
-            return NextResponse.next()
-        } catch {
-            const url = req.nextUrl.clone()
-            url.pathname = '/auth'
-            return NextResponse.redirect(url)
-        }
     }
 
     // Todas las demás rutas son públicas
