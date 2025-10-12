6617eda65055fd5f21fb3a88b5ecd130790e99e3
lautarodesouches <lautarodesouches@outlook.com>
2025-10-12 01:31:13 -0300

    [Fix] Suspense boundary

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -8,8 +8,8 @@ import {
     faUser,
     faUsers,
 } from '@fortawesome/free-solid-svg-icons'
-import { HomeFilter, HomeSocial, MapView } from '@/components'
-import { useState } from 'react'
+import { HomeFilter, HomeSocial, Loading, MapView } from '@/components'
+import { Suspense, useState } from 'react'
 import { MapProvider } from '@/components/Home/MapProvider'
 import Image from 'next/image'
 
@@ -28,111 +28,113 @@ export default function Map() {
     }
 
     return (
-        <main className={styles.main}>
-            <nav className={styles.nav}>
-                <div className={styles.nav__logo}>
-                    <Image
-                        src="/images/brand/gusto-center-negative.svg"
-                        alt="Logo Gusto!"
-                        className={styles.nav__img}
-                        width={0}
-                        height={0}
-                        priority
-                    />
-                </div>
-                <fieldset className={styles.nav__fieldset}>
-                    <FontAwesomeIcon
-                        icon={faSearch}
-                        className={styles.nav__icon}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Escribe un lugar"
-                        name="search"
-                        className={styles.nav__input}
-                    />
-                </fieldset>
-                <div className={styles.nav__icons}>
-                    <div className={styles.nav__div}>
-                        <FontAwesomeIcon
-                            icon={faBell}
-                            className={styles.nav__icon}
+        <Suspense fallback={<Loading message='Cargando mapa' />}>
+            <main className={styles.main}>
+                <nav className={styles.nav}>
+                    <div className={styles.nav__logo}>
+                        <Image
+                            src="/images/brand/gusto-center-negative.svg"
+                            alt="Logo Gusto!"
+                            className={styles.nav__img}
+                            width={0}
+                            height={0}
+                            priority
                         />
                     </div>
-                    <div className={styles.nav__div}>
+                    <fieldset className={styles.nav__fieldset}>
                         <FontAwesomeIcon
-                            icon={faUser}
+                            icon={faSearch}
                             className={styles.nav__icon}
                         />
+                        <input
+                            type="text"
+                            placeholder="Escribe un lugar"
+                            name="search"
+                            className={styles.nav__input}
+                        />
+                    </fieldset>
+                    <div className={styles.nav__icons}>
+                        <div className={styles.nav__div}>
+                            <FontAwesomeIcon
+                                icon={faBell}
+                                className={styles.nav__icon}
+                            />
+                        </div>
+                        <div className={styles.nav__div}>
+                            <FontAwesomeIcon
+                                icon={faUser}
+                                className={styles.nav__icon}
+                            />
+                        </div>
                     </div>
-                </div>
-            </nav>
-            <section className={styles.middle}>
-                <div className={styles.middle__filter}>
-                    <HomeFilter isVisible handleClose={() => {}} />
-                </div>
-                <MapProvider>
-                    <MapView />
-                </MapProvider>
-            </section>
-            <section className={styles.bottom}>
-                <div className={styles.bottom__container}>
-                    <button
-                        className={styles.bottom__button}
-                        onClick={handleClickFilters}
-                    >
-                        <FontAwesomeIcon
-                            icon={faFilter}
-                            className={styles.bottom__icon}
-                        />
-                        <span className={styles.bottom__span}>Filtros</span>
-                    </button>
-                    <button
-                        className={styles.bottom__button}
-                        onClick={handleClickSocial}
-                    >
-                        <FontAwesomeIcon
-                            icon={faUsers}
-                            className={styles.bottom__icon}
-                        />
-                        <span className={styles.bottom__span}>Social</span>
-                    </button>
-                </div>
-                <HomeFilter
-                    isVisible={isFiltersVisible}
-                    handleClose={() => {
-                        setIsFiltersVisible(!isFiltersVisible)
-                    }}
-                />
-                <HomeSocial
-                    isVisible={isSocialVisible}
-                    handleClose={() => {
-                        setIsSocialVisible(!isSocialVisible)
-                    }}
-                    friends={[
-                        {
-                            image: '',
-                            name: 'Lautaro Desoches',
-                            user: 'lauti',
-                        },
-                        {
-                            image: '',
-                            name: 'Agustin Cardeli',
-                            user: 'agus',
-                        },
-                    ]}
-                    groups={[
-                        {
-                            name: 'Trabajo',
-                            numberOfMembers: 7,
-                        },
-                        {
-                            name: 'Amigos',
-                            numberOfMembers: 5,
-                        },
-                    ]}
-                />
-            </section>
-        </main>
+                </nav>
+                <section className={styles.middle}>
+                    <div className={styles.middle__filter}>
+                        <HomeFilter isVisible handleClose={() => {}} />
+                    </div>
+                    <MapProvider>
+                        <MapView />
+                    </MapProvider>
+                </section>
+                <section className={styles.bottom}>
+                    <div className={styles.bottom__container}>
+                        <button
+                            className={styles.bottom__button}
+                            onClick={handleClickFilters}
+                        >
+                            <FontAwesomeIcon
+                                icon={faFilter}
+                                className={styles.bottom__icon}
+                            />
+                            <span className={styles.bottom__span}>Filtros</span>
+                        </button>
+                        <button
+                            className={styles.bottom__button}
+                            onClick={handleClickSocial}
+                        >
+                            <FontAwesomeIcon
+                                icon={faUsers}
+                                className={styles.bottom__icon}
+                            />
+                            <span className={styles.bottom__span}>Social</span>
+                        </button>
+                    </div>
+                    <HomeFilter
+                        isVisible={isFiltersVisible}
+                        handleClose={() => {
+                            setIsFiltersVisible(!isFiltersVisible)
+                        }}
+                    />
+                    <HomeSocial
+                        isVisible={isSocialVisible}
+                        handleClose={() => {
+                            setIsSocialVisible(!isSocialVisible)
+                        }}
+                        friends={[
+                            {
+                                image: '',
+                                name: 'Lautaro Desoches',
+                                user: 'lauti',
+                            },
+                            {
+                                image: '',
+                                name: 'Agustin Cardeli',
+                                user: 'agus',
+                            },
+                        ]}
+                        groups={[
+                            {
+                                name: 'Trabajo',
+                                numberOfMembers: 7,
+                            },
+                            {
+                                name: 'Amigos',
+                                numberOfMembers: 5,
+                            },
+                        ]}
+                    />
+                </section>
+            </main>
+        </Suspense>
     )
 }
