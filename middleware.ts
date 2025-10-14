4b87f014c3b534385220c2a40c18b9cb762bd35b
lautarodesouches <lautarodesouches@outlook.com>
2025-10-14 15:21:35 -0300

    [Fix] delete verify token in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { ROUTES } from './routes'
-import { verifyFirebaseToken } from './lib/firebaseAdmin'
 
-export async function middleware(req: NextRequest) {
+export function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl
 
     // Solo proteger la ruta /map
     if (pathname === '/map') {
         const token = req.cookies.get('token')?.value
 
-        if (!token || await verifyFirebaseToken(token)) {
+        if (!token) {
             const url = req.nextUrl.clone()
             url.pathname = ROUTES.LOGIN
             return NextResponse.redirect(url)
