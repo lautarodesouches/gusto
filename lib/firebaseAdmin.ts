5414cf599d3d63a96f432016f994639e859a646c
lautarodesouches <lautarodesouches@outlook.com>
2025-10-22 15:22:46 -0300

    [Fixing] Test fbadmin

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -2,6 +2,7 @@ import { FIREBASE_ADMIN_CONFIG } from '@/constants'
 import * as admin from 'firebase-admin'
 
 if (!admin.apps.length) {
+    console.log(process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'))
     admin.initializeApp({
         credential: admin.credential.cert(FIREBASE_ADMIN_CONFIG),
     })
